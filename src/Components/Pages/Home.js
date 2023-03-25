ddb9f4746926b204dbd7f25464509cf9ab2c96d2
Dharvik19 <dharvik.badga22@gmail.com>
2023-03-25 20:22:54 +0530

    task 3 complete profile

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 const Home =()=>{
     return(
         <>
-        <h1>Home.</h1>
+        <h1>Welcome to Expense tracker</h1>
+
+        <p> your profile is incomplete,<NavLink to='/home/profile'>complete your profile</NavLink></p>
         </>
+
     )
+
 }
 
 export default Home;
---
+++ src/Components/Pages/Profile.js
@@ -0,0 +1,77 @@
+import React,{useRef,useContext} from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import AuthContext from "../../FireBaseAuthentication/auth-context";
+const Profile =()=>{
+    const authCtx = useContext(AuthContext);
+    const nameRef = useRef();
+    const photoRef = useRef();
+    
+    const onSubmitHandler=(e)=>{
+        e.preventDefault();
+
+        const enteredName = nameRef.current.value;
+        const enteredImageUrl = photoRef.current.value;
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDp4Bq20RRH5ry4TmqXE2ScDwgc2wi3cHA',
+        {
+            method:'POST',
+            body:JSON.stringify({
+                idToken:authCtx.token,
+                displayName:enteredName,
+                displayPhoto:enteredImageUrl,
+
+                returnSecureToken:true,
+                Headers:{
+                    'Content-Type': "application/json"
+                }
+            })   
+        }).then((data)=>{
+            console.log(data)
+            console.log('seccess')
+          }).catch((err)=>{
+            alert(err.message);
+          })
+    }
+
+    return(
+        <>  
+            <Row>
+                <Col ><h3>Winners never quit , quiters never win</h3></Col>
+                <Col xs={4}> Your profile is 64% completes. A complete Profile has Higher chances
+          of landing a job.</Col>
+            </Row>
+            
+            
+            <hr></hr>
+            <form onSubmit={onSubmitHandler}>
+        <Container >
+            <Row>
+          <div>
+            <h2>Contact Details </h2>
+            
+          </div>
+          <div >
+            <div>
+              <label htmlFor="name">Full Name:</label>
+            </div>
+            <div>
+              <input type="text" ref={nameRef} ></input>
+            </div>
+            <div>
+              <label htmlFor="img"> Profile Photo URL</label>
+            </div>
+            <div>
+              <input type="text" ref={photoRef} ></input>
+            </div>
+          </div>
+          <div>
+            <Button >Update</Button>
+          </div>
+          <hr />
+          </Row>
+        </Container>
+      </form>
+        </>
+    )
+}
+export default Profile
