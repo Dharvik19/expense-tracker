cfc2d42a1f081c2e2f8e1db2e509a7aece7194a2
Dharvik19 <dharvik.badga22@gmail.com>
2023-04-22 19:54:25 +0530

    edit and delete expense added

--- src/Components/Expenses/ExpenseList.js
+++ src/Components/Expenses/ExpenseList.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import classes from './ExpenseList.module.css'
 const ExpenseList = (props) => {
   const enteredEmail = localStorage.getItem('email').replace('@','').replace('.','');
@@ -29,6 +29,39 @@ const ExpenseList = (props) => {
   useEffect(()=>{
     fetchDataHandler();
   },[fetchDataHandler]);
+
+  function deleteExpensehandler(id){
+    const response = fetch(`https://expense-tracker-d0790-default-rtdb.firebaseio.com/${enteredEmail}/${id}.json`,
+    {
+      method:"DELETE",
+    }
+    );
+    if(response.ok){
+      console.log("expense deleted");
+    }
+  }
+  const editExpense=(item)=>{
+    const filteredExpense = expense.filter((list)=>list.description === item.description);
+    for( let i = 0; i < expense.length; i++){ 
+
+      if ( expense[i] === filteredExpense[0]) { 
+
+          const amount = prompt('Enter the Amount', expense[i].amount);
+          const description  = prompt('Enter the Description', expense[i].description);
+          const select = prompt('Enter the Category', expense[i].select); 
+          expense.splice(i, 1,{amount:amount,description:description,select:select});
+      }
+      putRequest(expense).then(res=>{setExpense([...expense])})
+  }
+    console.log(filteredExpense);
+  }
+  async function putRequest(expenseList){
+    await fetch(`https://expense-tracker-d0790-default-rtdb.firebaseio.com/${enteredEmail}.json`, 
+    {
+      method:"PUT",
+      body:JSON.stringify(expenseList)
+    })
+  }
   return (
     <>
       <Container className={classes.container} >
@@ -40,7 +73,7 @@ const ExpenseList = (props) => {
                 className={classes.listItem}
                 // style={{ backgroundColor: "rgba( 255, 255, 255,0.7)",backdropFilter:"blur(10px)",WebkitBackdropFilter:"blur(10px)", color:"rgba(32, 16, 57,1)",borderBottom:"4px solid rgba(82, 40, 145, 1)", padding:"10px 20px", borderRadius:"10px", margin:"5px 0" }}
                 key={item.id}
-              >{`Category: ${item.select} -- Description: ${item.description} -- Price: ${item.amount}`}</li>
+              ><div className="row"><div className="col d-flex align-items-center">{`Category: ${item.select} -- Description: ${item.description} -- Price: ${item.amount}`}</div> <div className="col d-grid gap-2 d-md-flex justify-content-md-end"><Button variant="danger" onClick={()=>deleteExpensehandler(item.id)}>Delete</Button> <Button onClick={()=>{editExpense(item)}} variant="secondary">Edit</Button></div></div></li>
             ))}
           </ul>
         
